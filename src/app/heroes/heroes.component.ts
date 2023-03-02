0f8d439723c10c3c63674bc3c0d6b5d4bdc0ffd1
vthaihien1412 <v.thaihien1412@gmail.com>
2023-03-02 10:39:08 -0500

    push

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero' ;
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +7,23 @@ import { Hero } from '../hero' ;
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  constructor() {
+    const hero : Hero = {
+    id: 123, name: "hello"
+    }
+    const hero1 : Hero = {
+      id: 345, name: "hello1"
+    }
+    const hero2: Hero = {
+      id: 456, name: "hello2"
+    }
+    this.heroes = [hero, hero1, hero2];
+    this.selectedHero = hero2;
+  }
 
-  hero: Hero = {
-
-    id: 1,
-    name: "Windstorm"
-
-  };
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Dr.Nice' },
+  { id: 3, name: 'Bombasto'},
+  { id: 4, name: 'Celeritas'},
+  { id: 5, name: 'Magneta'},
+  { id: 6, name: 'RubberMan'},
+  { id: 7, name: 'Dynama'},
+  { id: 8, name: 'Dr. IQ'},
+  { id: 9, name: 'Magma'},
+  { id: 10, name: 'Tornado'}
+];
